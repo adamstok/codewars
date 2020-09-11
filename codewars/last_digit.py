622d0277f57659d7ab4980243348ebcb11b25faf
adamstok <adamstokowski@outlook.com>
2020-09-11 19:55:19 +0200

    timeout with big numbers

--- codewars/last_digit.py
+++ codewars/last_digit.py
@@ -11,4 +11,16 @@
 # last_digit(10, 10 ** 10)        # returns 0
 # last_digit(2 ** 200, 2 ** 300)  # returns 6
 
+def last_digit(n1,n2):
+    return int(str(n1**n2)[-1])
+
+# TODO: timeout with big numbers !
+
+
+assert last_digit(4, 1) ==  4
+assert last_digit(4, 2) ==  6
+assert last_digit(9, 7) ==  9
+assert last_digit(10, 10 ** 10) ==  0
+assert last_digit(2 ** 200, 2 ** 300) ==  6
+assert last_digit(3715290469715693021198967285016729344580685479654510946723, 68819615221552997273737174557165657483427362207517952651) ==  7
 
