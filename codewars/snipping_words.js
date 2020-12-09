ae050a129df51bd74699c1dcc57c654e52a87cf8
adamstok <adamstokowski@outlook.com>
2020-12-09 14:35:29 +0100

    snipped

--- codewars/snipping_words.js
+++ codewars/snipping_words.js
@@ -1,5 +1,16 @@
 let snipping = function(text){
-    console.log(text);
+    let outp = [];
+    let splitted = text.split(' ');
+    for (let i = 0; i < splitted.length; i++){
+        if (splitted[i].length < 5){
+            outp.push(splitted[i]);
+        } else {
+            let new_word = splitted[i].split('').reverse().join('');
+            outp.push(new_word);
+        }
+    }
+    console.log(outp.join(' '));
+    return outp.join(' ');
 }
 
-snipping('test');
+snipping('hello World abcdefghijk');
